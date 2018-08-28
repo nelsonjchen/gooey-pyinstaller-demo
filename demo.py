5f7a291a0352afe05a54b946b58fce336b058edb
Nelson Chen <crazysim@gmail.com>
2018-08-28 16:32:27 -0700

    Initial commit

---
+++ demo.py
@@ -0,0 +1,24 @@
+import argparse
+import time
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('name',
+                        action='store',
+                        help='Your name')
+    parser.add_argument('-n', '--number-of-os', type=int, default=5,
+                        help='Numbers of o\'s to add to your name')
+    args = parser.parse_args()
+    name = args.name
+    number_of_os = args.number_of_os
+    os_added = 0
+    while os_added < number_of_os:
+        time.sleep(1)
+        name += "o"
+        os_added = os_added + 1
+    print(f"Here's the name with some O's!: {name}")
+
+
+if __name__ == "__main__":
+    main()
